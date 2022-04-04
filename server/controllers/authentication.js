4243f5b031fae8e15d2b97cf783ff93ae2ab6317
Rob <robert.checco@gmail.com>
2022-04-03 20:46:38 -0400

    remove stale code in authentication.js

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -11,26 +11,22 @@ exports.signup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    // See if a user with a given email exists...
     User.findOne({ email: email}, (err, existingUser) => {
 
         if (!email || !password) {
-            return res.status(422).send({ error: 'You must provide a email and a password!!!'});
+            return res.status(422).send({ error: 'You must provide an email and a password.'});
         }
 
         if (err) { return next(err); }
         
-        // If a user with the email does exists
         if(existingUser) {
             return res.status(422).send({ error: 'Email is already in use' });
         }
 
-        // If a user with an email does not exist, create and save user record
-        const user = new User({ email: email, password: password }); // This is an in memory
+        const user = new User({ email: email, password: password });
 
         user.save(function(err) {
             if (err) { return next(err); }
-            // Respond to incoming request
             res.json({ token: tokenForUser(user) });
         });
     });
