1791a6be870d54f4d7c5e455206421e2785fbc17
Rob <robert.checco@gmail.com>
2022-04-03 20:26:44 -0400

    remove stale code from app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-// const mongoose = require('mongoose');
 const cors = require('cors');
-// const auth = require('./middleware/auth');
 
 const app = express();
 
@@ -19,12 +17,10 @@ require('./db');
 app.set('trust proxy', true);
 
 const userRouter = require('./router');
-// const adminRouter = require("./admin/admin");
 
 // App Setup
 app.use(morgan('combined')); // Middleware to log out requests
 app.use(bodyParser.json()); // Middleware to ???
 app.use(userRouter);
-// app.use(adminRouter);
 
 module.exports = app;
